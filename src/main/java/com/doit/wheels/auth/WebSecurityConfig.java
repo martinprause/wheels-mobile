e9ac2459edb7ba4285ab535f347cd19ac1285747
Denis Gorbach <denis.gorbach@do-it.co>
2017-08-01 16:36:23 +0300

    Added login controller.

--- src/main/java/com/doit/wheels/auth/WebSecurityConfig.java
+++ src/main/java/com/doit/wheels/auth/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
 @Configuration
@@ -11,9 +12,9 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
         http.authorizeRequests().anyRequest().fullyAuthenticated().and().
                 httpBasic().and().
                 csrf().disable();
     }
-
 }
--- src/main/java/com/doit/wheels/controllers/AbstractController.java
+++ src/main/java/com/doit/wheels/controllers/AbstractController.java
@@ -4,7 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600,
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS, RequestMethod.DELETE, RequestMethod.PUT},
+        allowedHeaders = {"x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN"})
 @RestController
 @RequestMapping(value = "/**")
 public abstract class AbstractController {
